6e7f7173205f1b0f645ef6b6a38edae3dee46681
stoertebeker2k <stoertebeker@kkb-clan.de>
2017-12-06 22:17:37 +0100

    initial project version

---
+++ README.md
@@ -0,0 +1,4 @@
+#Docker:CE installer script
+This script installs the latest version of docker community edition
+The code is the ubuntu installation from the docker doku: 
+https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#from https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-docker-ce-1
+
+#uninstall old versions
+sudo apt-get remove docker docker-engine docker.io
+
+sudo apt-get update
+
+sudo apt-get install \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+
+echo "git installed!"
